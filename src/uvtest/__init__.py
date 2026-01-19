8a978b08f8f93630b7b3f2e094c5850d536ef60f
Federaffo <federicoraffoni00@gmail.com>
2026-01-19 20:52:24 +0100

    dynamic version

--- src/uvtest/__init__.py
+++ src/uvtest/__init__.py
@@ -1,3 +1,6 @@
 """uvtest - A CLI tool to run pytest tests across all packages in a UV monorepo."""
 
-__version__ = "0.1.0"
+try:
+    from uvtest._version import __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"
